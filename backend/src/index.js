cb3d7844f956be5123feaf7db83e137f0074ffba
JulioBeas <juliobeas195@gmail.com>
2026-02-07 11:18:58 -0600

    feat. update main index to call db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,10 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
+import { connectSequelize, sequelize } from "./db/sequelize.js";
+import "./db/index.js";
+
+await connectSequelize();
+await sequelize.sync();
 
 app.listen(env.port, () => {
     console.log(`Personal Movie backend running on http://localhost:${env.port}`);
