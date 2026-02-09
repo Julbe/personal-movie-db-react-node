ed7de888e9d8409b303dccaeb82556558b8e4654
JulioBeas <juliobeas195@gmail.com>
2026-02-09 02:45:58 -0600

    feat. add Watchlist and recent comparisons sections to home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import RecentComparisonsSection from "../components/Comparsions/RecentComparisonsSection";
 import HeroSection from "../components/HeroSection/HeroSection";
+import WatchlistSection from "../components/Watchlist/WatchlistSection";
 
 
 export default function Home() {
     return (
-        <HeroSection />
+        <>
+            <HeroSection />
+            <WatchlistSection
+                onViewAll={() => console.log("Go to /watchlist")}
+                onSelectMovie={(movie) => console.log("Select", movie)}
+            />
+            <RecentComparisonsSection
+                onNewComparison={() => console.log("Go to /comparison page")}
+            />
+        </>
+
     );
 }
